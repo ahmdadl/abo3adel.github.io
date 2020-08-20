d1fbdccf7dfdd5487efd8302d6207bc7a8b2eaea
abo3adel <abo3adel35@gmail.com>
2020-08-20 14:32:28 +0100

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,101 @@
+export default {
+    /*
+     ** Nuxt rendering mode
+     ** See https://nuxtjs.org/api/configuration-mode
+     */
+    mode: 'spa',
+    /*
+     ** Nuxt target
+     ** See https://nuxtjs.org/api/configuration-target
+     */
+    target: 'static',
+    /*
+     ** Headers of the page
+     ** See https://nuxtjs.org/api/configuration-head
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || '',
+            },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+    /*
+     ** Global CSS
+     */
+    css: ['@\\assets\\css\\app.scss'],
+    /*
+     ** Plugins to load before mounting the App
+     ** https://nuxtjs.org/guide/plugins
+     */
+    plugins: [],
+    /*
+     ** Auto import components
+     ** See https://nuxtjs.org/api/configuration-components
+     */
+    components: true,
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: ['@nuxt/typescript-build'],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        'nuxt-i18n',
+        '@nuxtjs/auth',
+    ],
+
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {},
+    /*
+     ** Build configuration
+     ** See https://nuxtjs.org/api/configuration-build/
+     */
+    build: {},
+
+    /**
+     * I18n configuration
+     * See https://i18n.nuxtjs.org/options-reference.html
+     */
+    i18n: {
+        vueI18nLoader: false,
+        locales: [
+            { code: 'en', iso: 'en-US', file: 'en.js' },
+            { code: 'ar', iso: 'ar-EG', file: 'ar.js' },
+        ],
+        lazy: true,
+        langDir: 'lang/',
+        defaultLocale: 'en',
+        vueI18n: {
+            fallbackLocale: 'en',
+        },
+    },
+
+    /**
+     * auth config
+     */
+    auth: {
+        strategies: {
+            'laravel.passport': {
+                url: '...',
+                client_id: '...',
+                client_secret: '...',
+            },
+        },
+    },
+}
