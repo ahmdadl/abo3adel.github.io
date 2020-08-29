da8a0d024ef9e91899d4606635e59e7375b3d054
abo3adel <abo3adel35@gmail.com>
2020-08-29 01:27:20 +0100

    testing api jwt auth

---
+++ server/tests/Feature/LoginTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /** @var \App\User */
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /** @test */
+    public function authenticate()
+    {
+        $this->postJson('/api/auth/login', [
+            'email' => $this->user->email,
+            'password' => 'password',
+        ])
+            ->assertSuccessful()
+            ->assertJsonStructure(['token', 'expires_in'])
+            ->assertJson(['token_type' => 'bearer']);
+    }
+
+    /** @test */
+    public function fetch_the_current_user()
+    {
+        $this->actingAs($this->user)
+            ->getJson('/api/auth/me')
+            ->assertSuccessful()
+            ->assertJsonStructure(['id', 'name', 'email']);
+    }
+
+    /** @test */
+    public function log_out()
+    {
+        $this->withoutExceptionHandling();
+        $token = $this->postJson('/api/auth/login', [
+            'email' => $this->user->email,
+            'password' => 'password',
+        ])->json()['token'];
+
+        $this->postJson("/api/auth/logout?token=$token")
+            ->assertSuccessful();
+
+        // $this->getJson("/api/auth/user?token=$token")
+            // ->assertStatus(401);
+    }
+}
