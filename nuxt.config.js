d299b8e093b17510dd233311b6062b377261b134
abo3adel <abo3adel35@gmail.com>
2020-09-18 20:33:34 +0100

    add push-dir

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,11 @@ export default {
      ** See https://nuxtjs.org/api/configuration-target
      */
     target: 'static',
+
+    // router base for github pages
+    // router: {
+    //     base: '/<repository-name>/',
+    // },
     /*
      ** Headers of the page
      ** See https://nuxtjs.org/api/configuration-head
